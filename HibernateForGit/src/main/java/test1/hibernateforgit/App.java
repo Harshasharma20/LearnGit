fe05f9aed7a77dd2e15b4b8bd9353b96b9d647ce
Harsha sharma <harshasharma1968@gmail.com>
2021-01-06 18:57:07 +0530

    I made new project with name of HibernateForGit for learning git

---
+++ HibernateForGit/src/main/java/test1/hibernateforgit/App.java
@@ -0,0 +1,15 @@
+package test1.hibernateforgit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        
+        System.out.println( "Hello World!" );
+    }
+}
